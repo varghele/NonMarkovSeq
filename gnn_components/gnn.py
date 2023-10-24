1dadf99fc3e859df577e9f0179fec2e19a2dfd20
varghele <mgm.fischer@gmail.com>
2023-10-24 13:49:45 +0200

    updated GNN block

--- gnn_components/gnn.py
+++ gnn_components/gnn.py
@@ -1,62 +1,59 @@
 import torch
-from torch.nn import Sequential as Seq, Linear as Lin
+from torch.nn import LeakyReLU, BatchNorm1d
 from torch_geometric.nn import MetaLayer
 from edge_model import EdgeModel
 from mlp import MLP
 
 
 class GNN(torch.nn.Module):
-    def __init__(self, num_edge_feats, num_node_feats, num_global_feats, num_hid_layers, size_hid_layers,
-                 num_mlp_layers, size_mlp_layers, num_outputs, num_mp, device, activation=None, norm=None):
+    def __init__(self, num_edge_feats, num_node_feats, num_global_feats, num_mlp_layers, size_mlp_layers,
+                 activation = LeakyReLU, normalization = BatchNorm1d, num_mp, device):
         super(GNN, self).__init__()
 
+        # Assigning graph features
         self.num_edge_feats = num_edge_feats
         self.num_node_feats = num_node_feats
         self.num_global_feats = num_global_feats
 
-        self.num_hid_layers = num_hid_layers
-        self.size_hid_layers = size_hid_layers
-        self.activation = activation
-        self.norm = norm
+        # Assigning MLP definers
+        self.num_hid_layers = num_mlp_layers
+        self.size_hid_layers = size_mlp_layers
 
-        # Load MLP
+        # Create functions
+        self.activation = activation
+        self.normalization = normalization
+
+        # Create MLPs
+        self.edge_ml_node = MLP(self.num_node_feats, self.num_node_feats, self.num_hid_layers, self.size_hid_layers,
+                                activation=self.activation, normalization=self.normalization)
+        self.edge_mlp_all = MLP(self.num_edge_feats+self.num_edge_feats+self.num_global_feats, self.num_edge_feats,
+                                self.num_hid_layers, self.size_hid_layers, activation=self.activation,
+                                normalization=self.normalization)
 
         # Load Edge_model
-        self.edge_model = EdgeModel(num_edge_feats, num_node_feats, num_global_feats, edge_mlp_node, edge_mlp_all,
-                                    activation=None, norm=None)
+        self.edge_model = EdgeModel(num_edge_feats, num_node_feats, num_global_feats, self.edge_mlp_node,
+                                    self.edge_mlp_all)
 
-        self.num_mlp_layers = num_mlp_layers
-        self.size_mlp_layers = size_mlp_layers
-        self.num_outputs = num_outputs
-
+        # Message-passing steps
         self.num_mp = num_mp
 
+        # Device (GPU/CPU)
         self.device = device
 
+        # Define Metalayer (is the GNN block)
+        self.meta = MetaLayer(self.edge_model, None, None)
 
-
-        self.meta = MetaLayer(
-            EdgeModel(self.num_edge_feats, self.num_node_feats, self.num_hid_layers, self.size_hid_layers,
-                      self.activation, self.norm),
-            None,
-            None)
-
-        # Initialize MLP
-        self.last_mlp.apply(init_weights)
 
     def forward(self, grph):
         # Extract all from MiniBatch graph
-        x, ei, ea, btc = grph.x, grph.edge_index, grph.edge_attr, grph.batch
-
-        # Get batch size
-        batch_size = grph.y.size()[0]
-
-        # Create empty global feature
-        u = torch.full(size=(batch_size, self.num_global_feats), fill_value=0.1, dtype=torch.float).to(self.device)
+        x, ei, ea, u, btc = grph.x, grph.edge_index, grph.edge_attr, grph.y, grph.batch
 
         # Do message passing
         for _ in range(self.num_mp):
             x, ea, u = self.meta(x=x, edge_index=ei, edge_attr=ea, u=u, batch=btc)
 
+        # re-write info on graph
+        grph.edge_attr = ea
+
         # Run MLP on output
-        return self.last_mlp(u)
+        return grph
